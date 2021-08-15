c1ab5775178b89ef021aa71e947962b31793f580
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-08-15 15:43:44 -0500

    Connect to DB and retrieve product keys

--- main.go
+++ main.go
@@ -2,26 +2,62 @@ package main
 
 import (
 	"database/sql"
-	"os"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	args := os.Args
-	
-	if len(args) < 3 {
-		panic("Provide username and password")
-	}
-	username := args[1]
-	password := args[2]
-	db_name := "giftxtrade2"
+func connect_db(user string, pass string, db_name string) *sql.DB {
+	url := user + ":" + pass + "@tcp(localhost:3306)/" + db_name
 
-	db, err := sql.Open("mysql", username + ":" + password + "@tcp(127.0.0.1:3306)/" + db_name)
-    if err != nil {
-        panic(err.Error())
+	var db *sql.DB
+	var err error
+    if db, err = sql.Open("mysql", url); err != nil {
+        panic("Could not connect to db.\n" + err.Error())
     }
+	return db
+}
 
-    // defer the close till after the main function has finished executing
+func load_db_config() (map[string]string, error) {
+	db_config_file_data, err := ioutil.ReadFile("db_config.json")
+	if err != nil {
+		return map[string]string{}, errors.New("db_config.json file not found")
+	}
+
+	var db_config map[string]string
+	err = json.Unmarshal([]byte(db_config_file_data), &db_config)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return db_config, nil
+}
+
+func main() {
+	db_config, err := load_db_config()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db := connect_db(db_config["user"], db_config["pass"], db_config["db_name"])
     defer db.Close()
+
+
+	rows, err := db.Query("SELECT productKey from products")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	var keys []string = make([]string, 1000)
+	for rows.Next() {
+		var key string
+		if err = rows.Scan(&key); err != nil {
+			panic(err.Error())
+		}
+		keys = append(keys, key)
+	}
+	fmt.Println(keys)
 }
