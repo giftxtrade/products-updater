2a0ef70e69c939baaac8ab117afd0a6d6b48e4a3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-08-16 18:12:31 -0500

    Organize into folders

---
+++ src/config/db_config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+)
+
+func Connect(user string, pass string, db_name string) *sql.DB {
+	url := user + ":" + pass + "@tcp(localhost:3306)/" + db_name
+
+	var db *sql.DB
+	var err error
+    if db, err = sql.Open("mysql", url); err != nil {
+        panic("Could not connect to db.\n" + err.Error())
+    }
+	return db
+}
+
+func LoadConfig() (map[string]string, error) {
+	db_config_file_data, err := ioutil.ReadFile("db_config.json")
+	if err != nil {
+		return map[string]string{}, errors.New("db_config.json file not found")
+	}
+
+	var db_config map[string]string
+	err = json.Unmarshal([]byte(db_config_file_data), &db_config)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return db_config, nil
+}
---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/giftxtrade/products-updater/src/config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db_config, err := config.LoadConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db := config.Connect(db_config["user"], db_config["pass"], db_config["db_name"])
+    defer db.Close()
+
+
+	rows, err := db.Query("SELECT productKey FROM products")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	var keys []string = make([]string, 1000)
+	for rows.Next() {
+		var key string
+		if err = rows.Scan(&key); err != nil {
+			panic(err.Error())
+		}
+		keys = append(keys, key)
+	}
+	fmt.Println(keys)
+}
